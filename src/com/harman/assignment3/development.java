f3a4347049cf336d66ba402c29f774e86cd4f76a
SreeVarshan18 <varshan.sree18@gmail.com>
2022-03-16 14:42:19 +0530

    main

---
+++ src/com/harman/assignment3/development.java
@@ -0,0 +1,14 @@
+package com.harman.assignment3;
+
+import java.util.Scanner;
+
+public class development {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Integer cel,far;
+        System.out.println("Enter Fahrenheit ");
+        far = input.nextInt();
+
+    }
+}
